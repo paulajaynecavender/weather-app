253ffbf84dd4660bf2f8980ffd28f4d0f1285134
paula cavender <you@example.com>
2023-01-11 13:43:16 +0000

    styling tweaks

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -5,11 +5,9 @@ const Location = () => {
   const weather = useSelector(selectWeather);
   const description = weather.list[0].weather[0].description;
   return (
-    <div className="location">
-      <h2 className="textShadow">{weather.city.name}</h2>
-      <h3 className="textShadow">
-        {description.charAt(0).toUpperCase() + description.slice(1)}
-      </h3>
+    <div className="location box shadow">
+      <h2>{weather.city.name}</h2>
+      <h3>{description.charAt(0).toUpperCase() + description.slice(1)}</h3>
     </div>
   );
 };
