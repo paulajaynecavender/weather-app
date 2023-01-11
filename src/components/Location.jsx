34f85ebef464e8bb0405a75afd317354c6866055
paula cavender <you@example.com>
2023-01-11 14:07:56 +0000

    added country

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -6,7 +6,9 @@ const Location = () => {
   const description = weather.list[0].weather[0].description;
   return (
     <div className="location box shadow">
-      <h2>{weather.city.name}</h2>
+      <h2>
+        {weather.city.name}, {weather.city.country}
+      </h2>
       <h3>{description.charAt(0).toUpperCase() + description.slice(1)}</h3>
     </div>
   );
